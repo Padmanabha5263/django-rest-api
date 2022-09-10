4cd0fffbda97400221ea2acc1fc97598083fd3e8
Padmanabha5263 <npadmanabha319@gmail.com>
2022-09-10 23:01:10 +0530

    create operation completed for book, publication, author

--- library/book/urls.py
+++ library/book/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
+from django.views.generic import RedirectView
+
 from . import views
 
 
 urlpatterns = [
-    path('', views.books, name="books-view"),
-    path('<str:pk>/', views.bookDetile, name="perticular-book"),
+    path('books/', views.books, name="books"),
+    path('books/<str:pk>/', views.bookDetile, name="perticular-book"),
+
+    path('authors/', views.authors, name="authors"),
+    path('authors/<str:pk>/', views.author, name="perticular-author"),
+
+    path('publications/', views.publications, name="publications"),
+    path('publications/<str:pk>/', views.publication, name="perticular-publication"),
+
+    path('', RedirectView.as_view(url="books/"))
 ]
--- library/book/views.py
+++ library/book/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
 from . import models
 from .serializers import BookSerializer, AuthorSerializer, PublicationSerializer
@@ -9,14 +10,56 @@ from .serializers import BookSerializer, AuthorSerializer, PublicationSerializer
 
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def books(request):
-    books = models.Book.objects.all()
-    serializer = BookSerializer(books, many=True)
-    return Response(serializer.data)
+    if(request.method == "GET"):
+        books = models.Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
+    
 
 @api_view(['GET'])
 def bookDetile(request, pk):
-    books = models.Book.objects.get(pk=pk)
-    serializer = BookSerializer(books, many=False)
+    try:
+        books = models.Book.objects.get(pk=pk)
+        serializer = BookSerializer(books, many=False)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    except models.Book.DoesNotExist:
+        return Response({"message":"book does not exist"}, status=status.HTTP_404_NOT_FOUND)
+    
+
+
+
+
+@api_view(['GET'])
+def authors(request):
+    author = models.Author.objects.all()
+    serializer = AuthorSerializer(author, many=True)
     return Response(serializer.data)
+
+@api_view(['GET'])
+def author(request, pk):
+    try:
+        author = models.Author.objects.get(pk=pk)
+        serializer = AuthorSerializer(author, many=False)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    except models.Author.DoesNotExist:
+        return Response({"message":"Author doesn't exist"}, status=status.HTTP_404_NOT_FOUND)
+
+
+
+
+@api_view(['GET'])
+def publications(request):
+    pub = models.Publication.objects.all()
+    serializer = PublicationSerializer(pub, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def publication(request, pk):
+    try:
+        pub = models.Publication.objects.get(pk=pk)
+        serializer = PublicationSerializer(pub, many=False)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    except models.Publication.DoesNotExist:
+        return Response({"message":"Publication doesn't exist"}, status=status.HTTP_404_NOT_FOUND)
